a7ebc1da88c20ecfb49c5ecfc335d2b2725d8153
sashrikazelar <sashrika@zelarsoft.com>
2025-07-01 18:39:50 +0530

    usememo with search functionallity

--- src/components/Usememo.js
+++ src/components/Usememo.js
@@ -8,32 +8,82 @@
 
 //THIS IS AN EXAMPLE OF USEMEMO USING FACTORIAL .
 
-import React ,{useMemo, useState} from 'react'
+// import React ,{useMemo, useState} from 'react'
+
+// const Usememo = () => {
+//   const [count,setCount]= useState(0)
+//   const [number,setNumber] =useState(5)
+//   const factorialresult = useMemo(()=>{ return fact(number)},[number])
+//   return (
+//     <div>
+//       <h1>usememo</h1>
+//       <h3>factorial of {number} is {factorialresult}</h3>
+//       <button onClick={()=>setCount(count+1)}>Increment - {count}</button>
+//       <button onClick={()=>setNumber(number+1)}> click to trigger factorial only when number is changed</button>
+//      </div>
+//   )
+// }
+
+// const fact =(n)=>{
+// let result=1 
+
+// for (var i=n; i >=1 ; i--){
+//   result = result *i 
+// }
+// console.log("calling factorial function")
+// return result
+
+// }
+
+
+// export default Usememo
+
+
+//using usememo with searchfilter
+
+import React from 'react'
+import { useState, useEffect,useMemo } from 'react'
 
 const Usememo = () => {
-  const [count,setCount]= useState(0)
-  const [number,setNumber] =useState(5)
-  const factorialresult = useMemo(()=>{ return fact(number)},[number])
+  const [users, setUsers] = useState([]);
+  const [search,setSearch]=useState("")
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/photos')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+
+  }, [])
+
+  //withou usememo
+  const filteredData=()=>{
+    console.log("calling filter function")
+    return users.filter(user=>user.title.toLowerCase().includes(search.toLocaleLowerCase()))
+  }
+
+  const filteredDatausememo = useMemo(() => {
+    console.log("calling filter function with usememo")
+  return users.filter(user =>
+    user.title.toLowerCase().includes(search.toLowerCase())
+  );
+}, [search, users]);
+
   return (
     <div>
-      <h1>usememo</h1>
-      <h3>factorial of {number} is {factorialresult}</h3>
-      <button onClick={()=>setCount(count+1)}>Increment - {count}</button>
-      <button onClick={()=>setNumber(number+1)}> click to trigger factorial only when number is changed</button>
-     </div>
+      <input type="text" 
+      placeholder='search'
+      value={search}
+      onChange={e=>setSearch(e.target.value)}
+      />
+
+      {filteredDatausememo.map(user => (
+        <div key={user.id}>
+          <h3>{user.id}:{user.title}</h3>
+        </div>
+
+      ))}
+
+    </div>
   )
 }
-
-const fact =(n)=>{
-let result=1 
-
-for (var i=n; i >=1 ; i--){
-  result = result *i 
-}
-console.log("calling factorial function")
-return result
-
-}
-
 
 export default Usememo
